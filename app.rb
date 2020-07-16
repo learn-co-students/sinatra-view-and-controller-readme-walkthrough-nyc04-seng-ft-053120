de04a5f97e6e82c0ae8a78c417508899d3e971ba
Rouan Thompson <rouan.thompson@gmail.com>
2020-07-16 17:11:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,19 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    string = params[:string]
+    #instance variables are visible to erb files that are being rendered
+    #not visible to other controller methods in the controller(class App)
+    @reversed_string = string.reverse
+
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc',
+     'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
 end
